a8dea4a1d1cd835b0013143256d38cc8eb7facc2
Sofiane Abdellaoui <abdellaoui.sofiane.esb@gmail.com>
2021-07-02 15:43:13 +0200

    add get_category_url func that returns the url of a given category, add tests for it

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 BASE_URL = "http://books.toscrape.com/"
@@ -15,8 +16,16 @@ def get_category_list(soup):
 	ul = soup.find('ul', {'class': 'nav nav-list'}).find('ul')
 	return [category for category in ul.stripped_strings]
 
+def get_category_url(soup, category):
+	link = soup.find(
+		'a',
+		string=re.compile(rf'^\s*{category}\s*$')
+	)
+	return BASE_URL + link.get('href')
+
 
 if __name__ == "__main__":
 	soup = get_soup()
 	category_list = get_category_list(soup)
-	print(category_list)
+	for category in category_list:
+		print(get_category_url(soup, category))
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 import requests
+import re
 from bs4 import BeautifulSoup
+from slugify import slugify
 
 from scraper import *
 
@@ -9,6 +11,12 @@ class SoupTestCase(unittest.TestCase):
 
 	def setUp(self):
 		self.base_url = "http://books.toscrape.com/"
+
+	def build_category_url_regex(self, category, index):
+		category_slug = slugify(category)
+		return re.compile(
+			rf'^{self.base_url}catalogue/category/books/{category_slug}_{index}/index.html$'
+		)
 
 	def test_response_from_base_url(self):
 		response = requests.get(self.base_url)
@@ -23,6 +31,18 @@ class SoupTestCase(unittest.TestCase):
 		category_list = get_category_list(soup)
 		self.assertIsInstance(category_list, list)
 		self.assertEqual(len(category_list), 50)
+
+	def test_get_category_url(self):
+		soup = get_soup()
+		category_list = get_category_list(soup)
+		index = 2
+		for category in category_list:
+			category_url = get_category_url(soup, category)
+			self.assertRegex(
+				category_url,
+				self.build_category_url_regex(category, index)
+			)
+			index += 1
 
 
 if __name__ == '__main__':
