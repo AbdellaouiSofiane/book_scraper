4d845866243009504872d2e909c8811f053bc1cb
Sofiane Abdellaoui <abdellaoui.sofiane.esb@gmail.com>
2021-07-14 17:38:55 +0200

    add comments to code

--- scraper.py
+++ scraper.py
@@ -23,19 +23,23 @@ FIELDNAMES = [
 ]
 
 def get_soup_from_url(url=BASE_URL):
+	""" Return a BeatifulSoup object from a given url."""
 	response = requests.get(url)
 	return BeautifulSoup(response.content, features="html.parser")
 
 def get_category_list(soup):
+	""" Return the list of book categories in website."""
 	ul = soup.find('ul', class_='nav nav-list').find('ul')
 	return [category for category in ul.stripped_strings]
 
 def get_category_base_url(soup, category):
+	""" Return the base url for a given category."""
 	regex = re.compile(rf'^\s*{category}\s*$')
 	link = soup.find('a', string=regex).get('href')
 	return urljoin(BASE_URL, link)
 
 def get_next_page_url(soup, base_url):
+	""" Return next page url if paginated."""
 	next_page = soup.find('li', class_="next")
 	if next_page:
 		return urljoin(base_url, next_page.a.get("href"))
@@ -43,6 +47,7 @@ def get_next_page_url(soup, base_url):
 		return None
 
 def get_books_urls_by_category(soup, category):
+	""" Return the list of books urls for a given category."""
 	book_list = []
 	category_base_url = get_category_base_url(soup, category)
 	page = category_base_url
@@ -53,7 +58,7 @@ def get_books_urls_by_category(soup, category):
 			book_list.append(
 				urljoin(category_base_url, book.a.get('href'))
 			)
-		page = get_next_page_url(soup, category_base_url)
+		page = get_next_page_url(soup, category_base_url) # get next page if it exists.
 	return book_list
 
 def get_UPC(soup):
@@ -92,6 +97,7 @@ def get_rating(soup):
 	return soup.find('p', class_="star-rating").get('class')[1]
 
 def get_book_data(url):
+	""" Return a dict object for a given book."""
 	soup = get_soup_from_url(url)
 	return {
 		'product_page_url': url,
@@ -106,18 +112,22 @@ def get_book_data(url):
 	}
 
 def save_book_cover(data):
+	""" save book cover file."""
 	image_url = data.get('image_url')
-	ext = image_url.split('.')[-1]
+	extension = image_url.split('.')[-1]
 	name = slugify(data.get('title'))
-	filename = f'images/{name}.{ext}'
+	filename = f'images/{name}.{extension}'
 	urlretrieve(image_url, filename)
 
 
 if __name__ == "__main__":
 	soup = get_soup_from_url()
 	category_list = get_category_list(soup)
+
+	# Create destination files in necessery.
 	Path("data/").mkdir(parents=True, exist_ok=True)
 	Path("images/").mkdir(parents=True, exist_ok=True)
+
 	for category in category_list:
 		print(f'Writing csv file for category: {category}')
 		with open(f'data/{category}.csv', 'w', newline='') as csvfile:
@@ -126,5 +136,5 @@ if __name__ == "__main__":
 			for book_url in get_books_urls_by_category(soup, category):
 				data = get_book_data(book_url)
 				data['category'] = category
-				writer.writerow(data)
+				writer.writerow(data) # Write book infos into the csv file
 				save_book_cover(data)
